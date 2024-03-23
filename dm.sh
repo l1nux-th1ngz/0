2b0bd569c1714ae7ddb3aaddcd57712b7ed2b017
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-03-23 00:48:52 -0600

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-sudo apt install -y lightdm lightdm-gtk-greeter-settings
-sudo apt install -y lightdm-settings
-sudo apt install -y slick-greeter
+# Install LightDM and related packages
+sudo apt install -y lightdm lightdm-gtk-greeter-settings lightdm-settings slick-greeter
+
+# Enable LightDM service
 sudo systemctl enable lightdm
-systemctl set-default graphical.target
+
+# Set default target to graphical
+sudo systemctl set-default graphical.target
+
+# Enable LightDM service to start on boot
 sudo systemctl enable lightdm
+
+# Configure LightDM to remember the last logged-in username
+echo "[Seat:*]" | sudo tee -a /etc/lightdm/lightdm.conf.d/50-lightdm.conf
+echo "greeter-hide-users=false" | sudo tee -a /etc/lightdm/lightdm.conf.d/50-lightdm.conf
+echo "greeter-show-manual-login=true" | sudo tee -a /etc/lightdm/lightdm.conf.d/50-lightdm.conf
