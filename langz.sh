0a47cef5f666368af5a230ebac6a9fad8208714c
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-06-15 02:48:02 -0600

    langz.sh

---
+++ langz.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get -y install python3-full
+sudo apt-get -y install ruby-full
+sudo apt-get -y install npm
+sudo apt-get -y install rbenv
+sudo apt-get -y install uxplay
+sudo apt-get -y install python3-pipx
+sudo apt-get -y install pipenv
+sudo apt-get -y install ruby-build
+sudo apt-get -y install lua5.4
+sudo apt-get -y install luarocks
